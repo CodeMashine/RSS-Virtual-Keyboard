25167008eab325edff5c034b278fc9f30cf3fc0c
Peter <79867740+CodeMashine@users.noreply.github.com>
2023-04-22 21:38:29 +0300

    init : init develop branch & start settings

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict" ;
+import { Keyboard } from "./keyboardClass.js" ;
+import "./style.scss" ;
+
+
+let test = new Keyboard ;
+
+test.test() ;
+
+console.log("work") ;
---
+++ src/keyboardClass.js
@@ -0,0 +1,12 @@
+class Keyboard {
+    constructor(){
+        this.language = "RU" ;
+    }
+    test(){
+        console.log(this.language) ;
+    }
+}
+
+
+
+export { Keyboard } ;
